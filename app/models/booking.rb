e9302017b801510129e385f96ae69bb90e098c8a
Ryan Mansfield <ryanmansfield86@gmail.com>
2019-09-10 16:50:10 -0300

    Added validations to bookings

--- app/models/booking.rb
+++ app/models/booking.rb
@@ -1,4 +1,10 @@
 class Booking < ApplicationRecord
   belongs_to :photographer
   belongs_to :user
+  validates :date, presence: true
+  validates :photographer_id, uniqueness: true, presence: true
+  validates :user_id, uniqueness: true, presence: true
+  validates :price, presence: true
 end
+
+
