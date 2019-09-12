899b69de610c9f2f467cbb4d4a4928b7b681e3c0
Ryan Mansfield <ryanmansfield86@gmail.com>
2019-09-12 16:34:51 -0300

    Added date time format so that you can render date and time things

--- db/migrate/20190912180318_add_time_to_bookings.rb
+++ db/migrate/20190912180318_add_time_to_bookings.rb
@@ -1,5 +1,5 @@
 class AddTimeToBookings < ActiveRecord::Migration[5.2]
   def change
-    add_column :bookings, :time, :time
+    add_column :bookings, :time, :string
   end
 end
--- db/schema.rb
+++ db/schema.rb
@@ -18,12 +18,12 @@ ActiveRecord::Schema.define(version: 2019_09_12_180318) do
   create_table "bookings", force: :cascade do |t|
     t.integer "photographer_id"
     t.bigint "user_id"
-    t.date "date"
+    t.datetime "date"
     t.integer "price"
     t.datetime "created_at", null: false
     t.datetime "updated_at", null: false
     t.string "payment"
-    t.time "time"
+    t.string "time"
     t.index ["user_id"], name: "index_bookings_on_user_id"
   end
 
