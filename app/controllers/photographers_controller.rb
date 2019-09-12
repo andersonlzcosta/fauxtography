5203f3babc4d80ca8fac37e2bf5f93d3cb87c5ac
Anderson Costa <andersonlzcosta@gmail.com>
2019-09-12 12:14:59 -0300

    pundit on the project

--- app/controllers/photographers_controller.rb
+++ app/controllers/photographers_controller.rb
@@ -1,13 +1,16 @@
 class PhotographersController < ApplicationController
   skip_before_action :authenticate_user!, only: :show
   def index
+    @photographers = policy_scope(Photographer)
     @photographers = Photographer.where(nil) # Creates an "all" scope
     @photographers = @photographers.where("location ILIKE ?", "%#{params[:location]}%") if params[:location]
     @photographers = @photographers.where("style ILIKE ?", "%#{params[:style]}%") if params[:style]
+    authorize @photographers
   end
 
   def show
     @photographer = Photographer.find(params[:id])
+    authorize @photographer
     @booking = Booking.new
   end
 end
